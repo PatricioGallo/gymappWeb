aa6ab6c7206e5d641b469da6e69fb1443890c8ea
PGallo <pgallo@emtech.com.ar>
2026-08-26 00:47:56 -0300

    sacar el check por defecto

--- src/pages/rutins.ts
+++ src/pages/rutins.ts
@@ -145,7 +145,7 @@ function excBlockMarkup(): string {
       </div>
       <div class="exc-extra">
         <label><input type="checkbox" class="noWeightCheck"> Sin peso</label>
-        <label><input type="checkbox" class="mismoPesoCheck" checked> Mismo peso en todas las series</label>
+        <label><input type="checkbox" class="mismoPesoCheck"> Mismo peso en todas las series</label>
         <input type="text" class="notaInput" placeholder="Nota para este ejercicio (opcional)" maxlength="140">
       </div>
     </div>
